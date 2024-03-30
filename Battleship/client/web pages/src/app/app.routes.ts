b119318506042ea6539a448ad25fe26dce74a08a
davidedimarco <davide.dimarco5@studio.unibo.it>
2024-03-30 15:07:29 +0100

    deleted web pages

--- Battleship/client/web pages/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-{path: "initialGrid", loadComponent: () => import("./controllers/initial_grid.component").then((c) => c.InitialGridComponent)},
-{path: "battleGrid", loadComponent: () => import("./features/battle_grid.component").then((c) => c.BattleGridComponent)},
-  {path: "connectPlayer", loadComponent: () => import("./controllers/connect_player.component").then((c) => c.ConnectPlayerComponent)},
-  {path: "lobbies", loadComponent: () => import("./controllers/lobbies.component").then((c) => c.LobbiesComponent)},
-{path: "", redirectTo: "account", pathMatch: "full"}
-];
--- Battleship/client/web pages/src/app/models/Lobby.ts
+++
@@ -1,4 +0,0 @@
-export interface Lobby {
-  id: number,
-  name: string,
-}
